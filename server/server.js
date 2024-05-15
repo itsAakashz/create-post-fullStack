1d8d38e3e6533ee618de677ddc7172a939acb6c4
Aakash kumar <76447347+itsAakashz@users.noreply.github.com>
2024-05-15 17:23:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const { connect } = require('./db/connectionMongoDb');
 const userRoutes = require("./routes/userRoutes");
-const cors =require("cors")
-
-
+const cors = require("cors");
 
 // Create an Express app
 const app = express();
 const port = process.env.PORT || 80;
 
-
 // Custom CORS configuration
 const corsOptions = {
-  origin: 'https://create-post-full-stack-ws4c.vercel.app/', // Replace with your frontend domain
+  origin: 'https://create-post-full-stack-ws4c.vercel.app', // Ensure no trailing slash
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
@@ -24,13 +21,19 @@ app.use(cors(corsOptions));
 // Use JSON middleware
 app.use(express.json());
 
-//using imported mongo coonection route 
+//using imported mongo connection route 
 (async () => {
     await connect();
 })();
 
-//using imported rotes here
+//using imported routes here
 app.use(userRoutes);
+
+// Middleware to check CORS headers (for debugging purposes)
+app.use((req, res, next) => {
+  console.log('CORS headers:', res.get('Access-Control-Allow-Origin'));
+  next();
+});
 
 // Start the server
 app.listen(port, () => {
