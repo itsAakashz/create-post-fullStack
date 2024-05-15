a98324da1e325f55159e1914964d41494ed19a48
Aakash kumar <76447347+itsAakashz@users.noreply.github.com>
2024-05-15 15:03:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors =require("cors")
 
 // Create an Express app
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 
 // Custom CORS configuration
